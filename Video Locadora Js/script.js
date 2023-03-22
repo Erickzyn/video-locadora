b51f0291d9cdb0f39a4361177328f4efa3d5ddf3
Érick Michael <108885379+Erickzyn@users.noreply.github.com>
2023-03-22 12:42:30 -0300

    Add files via upload

---
+++ Video Locadora Js/script.js
@@ -0,0 +1,8 @@
+const titulo = document.getElementById("titulo")
+const duracao = document.getElementById("duracao")
+const converter = document.getElementById("converter")
+const info = document.getElementById("info")
+
+converter.addEventListener("click", () => {
+    const horas = 
+})
