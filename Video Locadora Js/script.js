07c88bc7e4b351d4be60d0077eaab0be059a0bfe
Érick Michael <108885379+Erickzyn@users.noreply.github.com>
2023-03-24 12:28:21 -0300

    Update script.js

--- Video Locadora Js/script.js
+++ Video Locadora Js/script.js
@@ -1,8 +1,16 @@
-const titulo = document.getElementById("titulo")
-const duracao = document.getElementById("duracao")
-const converter = document.getElementById("converter")
-const info = document.getElementById("info")
+const inTitle = document.getElementById('inTitle');
+const inDuraction = document.getElementById('inDuraction');
+const outTilte = document.getElementById('outTitle');
+const outDuraction = document.getElementById('outDuraction');
+const buttonConverter = document.getElementById('btnConverter');
 
-converter.addEventListener("click", () => {
-    const horas = 
-})
+buttonConverter.addEventListener('click', () => {
+    let tilte = inTitle.value;
+    let duraction = Number(inDuraction.value);
+
+    let hours = Math.floor(duraction / 60);
+    let minutes = duraction % 60;
+
+    outTilte.textContent = tilte;
+    outDuraction.textContent = `${hours} horas e ${minutes} minuto(s)`;
+});
